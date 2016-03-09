2b498aba71e9eb70549077b4817c7a8e84ffe3ad
Richard Thompson <richard@inviqa.com>
2016-03-09 16:50:42 +0000

    add checkout component

--- src/Payment/view/frontend/web/js/view/checkout-pay.js
+++ src/Payment/view/frontend/web/js/view/checkout-pay.js
@@ -8,8 +8,7 @@ define(
     [
         'jquery',
         "uiComponent",
-        'ko',
-        'amazonPayment'
+        'ko'
     ],
     function(
         $,
@@ -24,7 +23,45 @@ define(
             initialize: function () {
                 var self = this;
                 this._super();
-                console.log('hi');
+
+                window.onAmazonLoginReady = function() {
+                    amazon.Login.setClientId('amzn1.application-oa2-client.fe5d817cfb2b45dcaf1c2c15966454bb');
+                };
+
+                window.onAmazonPaymentsReady = function(){
+                    // render the button here
+                    var authRequest,
+                        loginOptions;
+
+                    OffAmazonPayments.Button("LoginWithAmazon", "A1BJXVS5F6XP", {
+                        type:  "PwA",
+                        color: "Gold",
+                        language: "en-GB",
+
+                        authorization: function() {
+                            loginOptions = {scope: "profile payments:widget payments:shipping_address"};
+                            authRequest = amazon.Login.authorize (loginOptions, "https://amazon-payment.dev/customer/account");
+                        },
+
+                        onError: function(error) {
+                            // your error handling code.
+                            // alert("The following error occurred: "
+                            //        + error.getErrorCode()
+                            //        + ' - ' + error.getErrorMessage());
+                        }
+                    });
+                };
+
+                setTimeout(function() {
+                    var my_awesome_script = document.createElement('script');
+
+                    my_awesome_script.setAttribute('src','https://static-eu.payments-amazon.com/OffAmazonPayments/uk/sandbox/lpa/js/Widgets.js');
+
+                    document.head.appendChild(my_awesome_script);
+
+                    console.log('new');
+                }, 800);
+
             }
         });
     }
