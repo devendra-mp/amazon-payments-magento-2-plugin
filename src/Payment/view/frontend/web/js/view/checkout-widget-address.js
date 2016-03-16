81208fcd8aa4c4142bba4bfa577a1b5a201119c3
Richard Thompson <richard@inviqa.com>
2016-03-16 14:49:36 +0000

    update address widget

--- src/Payment/view/frontend/web/js/view/checkout-widget-address.js
+++ src/Payment/view/frontend/web/js/view/checkout-widget-address.js
@@ -1,7 +1,3 @@
-/**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
 /*global define*/
 
 define(
@@ -12,6 +8,7 @@ define(
         'Magento_Customer/js/model/customer',
         'Magento_Checkout/js/model/quote',
         'Magento_Checkout/js/action/select-shipping-address',
+        'Magento_Checkout/js/model/shipping-rate-processor/new-address',
         'amazonCore'
     ],
     function(
@@ -21,14 +18,18 @@ define(
         customer,
         quote,
         selectShippingAddress,
+        shippingProcessor,
         amazonCore
     ) {
         'use strict';
-
         var self;
         return Component.extend({
             defaults: {
                 template: 'Amazon_Payment/checkout-widget-address'
+            },
+            options: {
+                sellerId: 'A1BJXVS5F6XP',
+                addressWidgetDOMId: 'addressBookWidgetDiv'
             },
             isCustomerLoggedIn: customer.isLoggedIn,
             isAmazonAccountLoggedIn: ko.observable(false),
@@ -43,47 +44,78 @@ define(
                 this.setupAddressWidget();
                 amazonCore._loadAmazonWidgetsScript();
             },
-            checkAmazonLoggedIn: function() {
-                var loginOptions = {scope: "profile payments:widget payments:shipping_address", popup: true, interactive: 'never' };
-
+            /**
+             * Check to see whether user is currently logged into Amazon
+             */
+            verifyAmazonLoggedIn: function() {
+                var loginOptions = {
+                    scope: "profile payments:widget payments:shipping_address",
+                    popup: true,
+                    interactive: 'never'
+                };
                 amazon.Login.authorize (loginOptions, function(response) {
+                    console.log(response);
                     if(!response.error) {
                         self.isAmazonAccountLoggedIn(true);
                     }
                 });
-
             },
+            /**
+             * Setup events and bindings for the Amazon Address widget
+             */
             setupAddressWidget: function() {
                 window.onAmazonPaymentsReady = function() {
-                    self.renderAddressWidget();
-                    self.checkAmazonLoggedIn();
+                    self.isAmazonAccountLoggedIn.subscribe(function(value) {
+                        if(value) {
+                            setTimeout(function() {
+                                self.renderAddressWidget();
+                            },0);
+                        }
+                    });
+                    self.verifyAmazonLoggedIn();
                 }
             },
+            /**
+             * render Amazon address Widget
+             */
             renderAddressWidget: function() {
-                //subscribe to loggedIn value
-                this.isAmazonAccountLoggedIn.subscribe(function(value) {
-                    if(value) {
-                        setTimeout(function() {
-                            new OffAmazonPayments.Widgets.AddressBook({
-                                sellerId: 'A1BJXVS5F6XP',
-                                onOrderReferenceCreate: function(orderReference) {
-                                    console.log('onOrderReferenceCreate fired');
-                                    orderReference.getAmazonOrderReferenceId();
-                                },
-                                onAddressSelect: function(orderReference) {
-                                    console.log('onAddressSelect fired');
-                                },
-                                design: {
-                                    designMode: 'responsive'
-                                },
-                                onError: function(error) {
-                                    // your error handling code
-                                }
-                            }).bind('addressBookWidgetDiv');
-                        }, 2000);
-
+                new OffAmazonPayments.Widgets.AddressBook({
+                    sellerId: self.options.sellerId,
+                    onOrderReferenceCreate: function(orderReference) {
+                        orderReference.getAmazonOrderReferenceId();
+                    },
+                    onAddressSelect: function(orderReference) {
+                        //need to call GetOrderReferenceDetails (PHP) so need to do a proxy
+                        //ajax call which sends the orderReference and gets back the address
+                        //once we have the address we need to set it via the quote model
+                        //then call the below function via the shippingProcessor in order
+                        //to get the new rates based on the address
+                        console.log('addy select');
+                        var addy = quote.shippingAddress();
+                        addy.country_id = 'GB';
+                        quote.shippingAddress(addy);
+                        //shippingProcessor.getRates(self.getCurrentShippingAddress());
+                    },
+                    design: {
+                        designMode: 'responsive'
+                    },
+                    onError: function(error) {
+                        // your error handling code
                     }
-                });
+                }).bind(self.options.addressWidgetDOMId);
+            },
+            /**
+             * Get the current Shipping address set in the quote model
+             */
+            getCurrentShippingAddress: function() {
+                return quote.shippingAddress();
+            },
+            /**
+             * Set the shipping address in the quote model
+             * @param address
+             */
+            setCurrentShippingAddress: function(address) {
+                quote.shippingAddress(address);
             }
         });
     }
