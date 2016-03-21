a78cdcf32f50391ccf8ae849f7d5b13c6e2a2047
Richard Thompson <richard@inviqa.com>
2016-03-21 11:53:39 +0000

    remove amazon shipping file

--- src/Payment/view/frontend/web/js/view/amazon-shipping.js
+++
@@ -1,9 +0,0 @@
-define(['Magento_Checkout/js/view/shipping'], function (shippingComponent) {
-    'use strict';
-
-    return shippingComponent.extend({
-        validateShippingInformation: function() {
-            console.log('use this');
-        }
-    });
-});
