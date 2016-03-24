0c111ceb34dbe37d6881200bad28c4d47b5eefbc
Richard Thompson <zaylril@techtrix.net>
2016-03-24 17:32:51 +0000

    Update storage.js
    removing the hack to make amazon logged in always true.

--- src/Payment/view/frontend/web/js/model/storage.js
+++ src/Payment/view/frontend/web/js/model/storage.js
@@ -14,7 +14,7 @@ define(
         'use strict';
 
         var isCustomerLoggedIn = customer.isLoggedIn,
-            isAmazonAccountLoggedIn = ko.observable(true),
+            isAmazonAccountLoggedIn = ko.observable(false),
             isAmazonEnabled = ko.observable(window.checkoutConfig.payment.amazonPayment.isEnabled),
             orderReference,
             addressConsentToken = amazonCore.accessToken,
