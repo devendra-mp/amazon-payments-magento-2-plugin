7b0221b6264a1eda5a3bdad8c061ef50087f551e
jamescowie <james@jcowie.co.uk>
2016-03-25 15:37:53 +0000

    Start creating new payment method and process order

---
+++ src/Payment/Model/Method/Amazon.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Amazon\Payment\Model\Method;
+
+use Magento\Framework\Exception\LocalizedException;
+use Psr\Log\LoggerInterface;
+
+class Amazon extends \Magento\Payment\Model\Method\AbstractMethod
+{
+    const PAYMENT_METHOD_CODE = 'amazon_payment';
+
+    protected $_code = self::PAYMENT_METHOD_CODE;
+
+    protected $_canCapture = true;
+
+    protected $_canAuthorize = true;
+
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Payment\Model\Method\Logger $logger,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepositoryInterface,
+        LoggerInterface $generalLogger,
+        \Magento\Framework\Math\Random $random,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $scopeConfig,
+            $logger,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+    }
+
+    public function capture(\Magento\Payment\Model\InfoInterface $payment, $amount)
+    {
+        
+    }
+}
