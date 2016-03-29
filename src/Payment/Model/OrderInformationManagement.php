0f0af458796c339e3e937df75e631f4372bac01d
matt <mcurran@inviqa.com>
2016-03-29 13:40:00 +0100

    AMAZON-147 add in additional data

--- src/Payment/Model/OrderInformationManagement.php
+++ src/Payment/Model/OrderInformationManagement.php
@@ -5,7 +5,10 @@ namespace Amazon\Payment\Model;
 use Amazon\Core\Client\ClientFactoryInterface;
 use Amazon\Payment\Api\Data\QuoteLinkInterfaceFactory;
 use Amazon\Payment\Api\OrderInformationManagementInterface;
+use Amazon\Payment\Helper\Data;
 use Magento\Checkout\Model\Session;
+use Magento\Framework\AppInterface;
+use Magento\Quote\Model\Quote;
 use PayWithAmazon\ResponseInterface;
 
 class OrderInformationManagement implements OrderInformationManagementInterface
@@ -23,16 +26,22 @@ class OrderInformationManagement implements OrderInformationManagementInterface
     /**
      * @var QuoteLinkInterfaceFactory
      */
-    private $quoteLinkFactory;
+    protected $quoteLinkFactory;
+    /**
+     * @var Data
+     */
+    private $paymentHelper;
 
     public function __construct(
         Session $session,
         ClientFactoryInterface $clientFactory,
-        QuoteLinkInterfaceFactory $quoteLinkFactory
+        QuoteLinkInterfaceFactory $quoteLinkFactory,
+        Data $paymentHelper
     ) {
         $this->session          = $session;
         $this->clientFactory    = $clientFactory;
         $this->quoteLinkFactory = $quoteLinkFactory;
+        $this->paymentHelper    = $paymentHelper;
     }
 
     /**
@@ -42,20 +51,38 @@ class OrderInformationManagement implements OrderInformationManagementInterface
     {
         $quote = $this->session->getQuote();
 
+        $this->setReservedOrderId($quote);
+
+        $data = [
+            'amazon_order_reference_id' => $amazonOrderReferenceId,
+            'amount'                    => $quote->getGrandTotal(),
+            'currency_code'             => $quote->getQuoteCurrencyCode(),
+            'seller_order_id'           => $quote->getReservedOrderId(),
+            'store_name'                => $quote->getStore()->getName(),
+            'custom_information'        =>
+                'Magento Version : ' . AppInterface::VERSION . ' ' .
+                'Plugin Version : ' . $this->paymentHelper->getModuleVersion()
+            ,
+            'platform_id'               => $this->paymentHelper->getMerchantId()
+        ];
+
         /**
          * @var ResponseInterface $response
          */
-        $response = $this->clientFactory->create()->setOrderReferenceDetails(
-            [
-                'amazon_order_reference_id' => $amazonOrderReferenceId,
-                'amount'                    => $quote->getGrandTotal(),
-                'currency_code'             => $quote->getQuoteCurrencyCode()
-            ]
-        );
+        $response = $this->clientFactory->create()->setOrderReferenceDetails($data);
 
         $this->updateQuoteLink($quote->getId(), $amazonOrderReferenceId);
 
         return true;
+    }
+
+    protected function setReservedOrderId(Quote $quote)
+    {
+        if ( ! $quote->getReservedOrderId()) {
+            $quote
+                ->reserveOrderId()
+                ->save();
+        }
     }
 
     protected function updateQuoteLink($quoteId, $amazonOrderReferenceId)
