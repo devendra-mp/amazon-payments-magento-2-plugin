3b97603d8e5b5c5e6551d2090b49530a7bb5f5ba
Gary Mawdsley <gmawdsley@inviqa.com>
2016-03-31 17:08:41 +0100

    AMAZON-167: Adding the display of the selected shipping addres to the the payment step, below the payment widget

--- src/Payment/view/frontend/web/js/view/payment/method-renderer/amazon-payment-widget.js
+++ src/Payment/view/frontend/web/js/view/payment/method-renderer/amazon-payment-widget.js
@@ -4,6 +4,7 @@ define(
         'Magento_Checkout/js/view/payment/default',
         'ko',
         'Magento_Customer/js/model/customer',
+        'Magento_Customer/js/customer-data',
         'Magento_Checkout/js/model/quote',
         'Amazon_Payment/js/model/storage'
     ],
@@ -12,12 +13,14 @@ define(
         Component,
         ko,
         customer,
+        customerData,
         quote,
         amazonStorage
     ) {
         'use strict';
 
-        var self;
+        var self,
+            countryData = customerData.get('directory-data');
 
         return Component.extend({
             defaults: {
@@ -31,6 +34,7 @@ define(
             isCustomerLoggedIn: amazonStorage.isCustomerLoggedIn,
             isAmazonAccountLoggedIn: amazonStorage.isAmazonAccountLoggedIn,
             isAmazonEnabled: ko.observable(window.checkoutConfig.payment.amazonPayment.isEnabled),
+            address: quote.shippingAddress,
             initialize: function () {
                 self = this;
                 this._super();
@@ -62,6 +66,12 @@ define(
             },
             isActive: function() {
                 return true;
+            },
+            getCountryName: function(countryId) {
+                return (countryData()[countryId] != undefined) ? countryData()[countryId].name : "";
+            },
+            checkCountryName: function(countryId) {
+                return (countryData()[countryId] != undefined);
             }
         });
     }
