16f377db3ebc34d63046852bc0d928a268491179
matt <mcurran@inviqa.com>
2016-04-22 17:46:11 +0100

    AMAZON-21 confirm order handling

--- src/Payment/Observer/ConfirmOrder.php
+++ src/Payment/Observer/ConfirmOrder.php
@@ -2,6 +2,7 @@
 
 namespace Amazon\Payment\Observer;
 
+use Amazon\Payment\Api\Data\QuoteLinkInterface;
 use Amazon\Payment\Api\Data\QuoteLinkInterfaceFactory;
 use Amazon\Payment\Model\OrderInformationManagement;
 use Magento\Framework\Event\Observer;
@@ -46,18 +47,31 @@ class ConfirmOrder implements ObserverInterface
             $amazonOrderReferenceId = $quoteLink->getAmazonOrderReferenceId();
 
             if ($amazonOrderReferenceId) {
-                $saveOrderInformation = $this->orderInformationManagement->saveOrderInformation($amazonOrderReferenceId);
-
-                if (!$saveOrderInformation) {
-                    throw new RemoteServiceUnavailableException();
-                }
-
-                $confirmOrderReference = $this->orderInformationManagement->confirmOrderReference($amazonOrderReferenceId);
-
-                if (!$confirmOrderReference) {
-                    throw new RemoteServiceUnavailableException();
-                }
+                $this->saveOrderInformation($quoteLink, $amazonOrderReferenceId);
+                $this->confirmOrderReference($quoteLink, $amazonOrderReferenceId);
             }
         }
+    }
+
+    protected function saveOrderInformation(QuoteLinkInterface $quoteLink, $amazonOrderReferenceId)
+    {
+        if (!$quoteLink->isConfirmed()) {
+            $saveOrderInformation = $this->orderInformationManagement->saveOrderInformation($amazonOrderReferenceId);
+
+            if (!$saveOrderInformation) {
+                throw new RemoteServiceUnavailableException();
+            }
+        }
+    }
+
+    protected function confirmOrderReference(QuoteLinkInterface $quoteLink, $amazonOrderReferenceId)
+    {
+        $confirmOrderReference = $this->orderInformationManagement->confirmOrderReference($amazonOrderReferenceId);
+
+        if ( ! $confirmOrderReference) {
+            throw new RemoteServiceUnavailableException();
+        }
+
+        $quoteLink->setConfirmed(true)->save();
     }
 }
