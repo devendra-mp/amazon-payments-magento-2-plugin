be91681213844ac090a92f92c4c4ebff63a33221
Gary Mawdsley <gmawdsley@inviqa.com>
2016-04-27 11:34:12 +0100

    AMAZON-249: Amending the isBadgeEnabled config check for the Amazon Payment badge display

--- src/Core/Block/Config.php
+++ src/Core/Block/Config.php
@@ -62,7 +62,7 @@ class Config extends Template
      */
     public function isBadgeEnabled()
     {
-        return ($this->coreHelper->isPwaEnabled() || $this->coreHelper->isLwaEnabled());
+        return ($this->coreHelper->isPwaEnabled());
     }
 
     /**
