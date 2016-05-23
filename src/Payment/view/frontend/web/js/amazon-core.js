034ac3df78bf0a031a618314b90b37ff341a3ac5
Richard Thompson <richard@inviqa.com>
2016-05-23 12:12:02 +0100

    add state cache cookies

--- src/Payment/view/frontend/web/js/amazon-core.js
+++ src/Payment/view/frontend/web/js/amazon-core.js
@@ -6,33 +6,55 @@ define([
 ], function($, ko) {
     "use strict";
 
+    var clientId = window.amazonPayment.clientId,
+        amazonDefined = ko.observable(false),
+        accessToken = ko.observable(null);
 
+    /**
+     * getURLParamater from URL for access OAuth Token
+     * @param name
+     * @param source
+     * @returns {string|null}
+     */
     function getURLParameter(name, source) {
         return decodeURIComponent((new RegExp('[?|&|#]' + name + '=' +
                 '([^&]+?)(&|#|;|$)').exec(source) || [,""])[1].replace(/\+/g,
                 '%20')) || null;
     }
 
-    var hi = getURLParameter("access_token", location.hash);
-    if (typeof hi === 'string' && hi.match(/^Atza/)) {
-        $.cookieStorage.set('amazon_Login_accessToken', hi);
-        console.log(hi);
-        //window.location = 'https://amazon-payment.dev/amazon/login/authorize/'  + '?access_token=' + hi;
+    /**
+     * Set State Cache Auth Cookies if they aren't already set
+     * @returns {boolean}
+     */
+    function setAuthStateCookies() {
+        var token = getURLParameter("access_token", location.hash);
+        //return false is the cookies are already set and token is null
+        if ($.cookieStorage.get('amazon_Login_state_cache') !== null && $.cookieStorage.get('amazon_Login_accessToken') !== null && token === null) {
+            return false;
+        }
+        var newObj = {
+            access_token: token,
+            max_age: getURLParameter('expires_in', location.hash),
+            expiration_date: new Date().getTime() + (getURLParameter('expires_in', location.hash) * 1000),
+            client_id: "<?php echo $config['client_id'] ?>",
+            scope: getURLParameter('scope', location.hash)
+        };
+
+        if (typeof token === 'string' && token.match(/^Atza/)) {
+            $.cookieStorage.set('amazon_Login_state_cache', JSON.stringify(newObj));
+            $.cookieStorage.set('amazon_Login_accessToken', token);
+        }
+
     }
 
-    //console.log($.cookieStorage.get('amazon_Login_accessToken'));
-
-
-    var clientId = window.amazonPayment.clientId,
-        amazonDefined = ko.observable(false),
-        accessToken = ko.observable(null);
-        
+    /**
+     * Set Client ID
+     * @param cid
+     */
     function setClientId(cid) {
+        setAuthStateCookies();
         amazonDefined(true);
         amazon.Login.setClientId(cid);
-        if(window.location.protocol === 'http:') {
-            amazon.Login.setUseCookie(true);
-        }
     }
 
     if(typeof amazon === 'undefined') {
@@ -56,20 +78,19 @@ define([
             };
 
             return new Promise(function(resolve, reject) {
-                if(window.location.protocol === 'https:') {
+                var authCookie = $.cookieStorage.get('amazon_Login_accessToken');
+                if(authCookie !== null) {
+                    amazon.Login.retrieveProfile(authCookie, function(response){
+                        accessToken(authCookie);
+                        return !response.error ? resolve(!response.error) : reject(response.error);
+                    });
+                    //if no cookie is set (i.e. come from redirect)
+                } else {
                     amazon.Login.authorize (loginOptions, function(response) {
                         accessToken(response.access_token);
                         return !response.error ? resolve(!response.error) : reject(response.error);
                     });
-                } else if(window.location.protocol === 'http:') {
-                    var cookieAccessToken = $.cookieStorage.get('amazon_Login_accessToken');
-                    if(cookieAccessToken !== '') {
-                        accessToken(cookieAccessToken);
-                        return true;
-                    }
-                    return false;
                 }
-
             }).catch(function(e) {
                 console.log('error: ' + e);
             });
