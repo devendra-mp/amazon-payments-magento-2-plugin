7133cf7b111be02d3d20af5141e855695c6442f0
Richard Thompson <richard@inviqa.com>
2016-06-14 09:00:36 +0100

    add refactored checkout

--- src/Login/view/frontend/requirejs-config.js
+++ src/Login/view/frontend/requirejs-config.js
@@ -1,8 +1,7 @@
 var config = {
     map: {
         '*': {
-            amazonLogout: 'Amazon_Login/js/amazon-logout',
-            'Magento_Checkout/js/view/form/element/email': 'Amazon_Login/js/view/form/element/email'
+            amazonLogout: 'Amazon_Login/js/amazon-logout'
         }
     }
 };
